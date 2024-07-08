feb75ec6cd0e6b87358066193e899e99c3172e68
SPARSH RAJPUT <rajputsparsh10sr@gmail.com>
2024-07-08 20:20:38 +0530

    Update new.js

--- new.js
+++ new.js
@@ -4,8 +4,8 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; //playerX, playerO
-let count = 0; //To Track Draw
+let turnO = true;
+let count = 0; 
 
 const winPatterns = [
   [0, 1, 2],
